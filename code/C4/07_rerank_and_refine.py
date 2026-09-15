45031cd03d1e22699067d526c322618faca9b92d
Guancheng Wang <24189100307@stu.xidian.edu.cn>
2026-09-15 14:33:27 +0800

    fix: update deprecated import in code/C4/07_rerank_and_refine.py

--- code/C4/07_rerank_and_refine.py
+++ code/C4/07_rerank_and_refine.py
@@ -3,7 +3,7 @@ from langchain_community.vectorstores import FAISS
 from langchain.retrievers import ContextualCompressionRetriever
 from langchain.retrievers.document_compressors import LLMChainExtractor
 from langchain_community.embeddings import HuggingFaceBgeEmbeddings
-from langchain.text_splitter import RecursiveCharacterTextSplitter
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import TextLoader
 from langchain_deepseek import ChatDeepSeek
 
